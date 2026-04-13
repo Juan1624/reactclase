b175686d253939bcf1b10df3521538d6d6229440
Juan1624 <juanestebanarias164@gmail.com>
2026-04-13 21:26:11 +0000

    clse 13/ 04/26

--- src/equipo/index.tsx
+++ src/equipo/index.tsx
@@ -1,9 +1,55 @@
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
 import "./style.css";
 
+interface TeamData {
+  team: {
+    name: string;
+    info: {
+      city: string;
+      founded: string;
+      stadium: string;
+      president: string;
+      last_title: string;
+    };
+    ranking: {
+      position: string;
+      competition: string;
+    };
+    social: {
+      facebook: string;
+      instagram: string;
+      x: string;
+    };
+    links: {
+      store: string;
+      tickets: string;
+    };
+  };
+}
+
 function Equipo(){
+     const [data, setData] = useState<TeamData | null>(null);
+
+      useEffect(() => {
+            const fetchData = async () => {
+                try {
+                    const res = await fetch('https://raw.githubusercontent.com/sdtibata/dataliga/refs/heads/main/${equipo}.json')
+                    const data = await res.json()
+                    
+                    setData(data.standings[0].ranking)
+                } catch (error) {
+                    console.error('Error cargando datos:', error)
+                }
+            }
+    
+            fetchData()
+        }, [])
+    const {equipo} = useParams <{ equipo: string}>()
     return (
+        
     <>
-    <p>equipo</p>
+    <p>{equipo}</p>
     </>
     )
 }
